3a25e8dcd1bf8597776822d71c70e2d42c3d2230
derekwesolowski <54447485+derekwesolowski@users.noreply.github.com>
2025-05-09 13:26:34 -0500

    Update variables.tf

--- .github/workflows/terraform/variables.tf
+++ .github/workflows/terraform/variables.tf
@@ -1,27 +1,40 @@
-variable "project_id" {
-  description = "The GCP project ID"
+variable "org_id" {
+  default     = "176230173478"
+  description = "Organization ID"
   type        = string
 }
 
-variable "organization_domain" {
-  description = "The organization domain (e.g., example.com)"
-  type        = string
-}
-
-variable "perimeter_name" {
-  description = "Name for the service perimeter"
-  type        = string
-  default     = "vpc_sc_perimeter"
-}
-
-variable "create_access_level" {
-  description = "Whether to create an access level"
-  type        = bool
-  default     = false
-}
-
-variable "allowed_ip_subnetworks" {
-  description = "List of IP subnetworks allowed in the access level"
+variable "client_projects" {
+  default     = ["client1-hipaa-20250509033526", "client2-hipaa-20250509033526"]
+  description = "HIPAA client project IDs"
   type        = list(string)
-  default     = []
+}
+
+variable "ip_subnetworks" {
+  default     = ["203.0.113.0/32"]
+  description = "Allowed IP ranges"
+  type        = list(string)
+}
+
+variable "os_constraints" {
+  default = [
+    { osType = "DESKTOP_WINDOWS", osVersion = "" },
+    { osType = "DESKTOP_MAC",     osVersion = "" },
+  ]
+  description = "Allowed device OS types"
+  type = list(object({
+    osType    = string
+    osVersion = string
+  }))
+}
+
+variable "restricted_services" {
+  description = "Google Cloud services to restrict with VPC SC"
+  type        = list(string)
+  default = [
+    "bigquery.googleapis.com",
+    "storage.googleapis.com",
+    "cloudfunctions.googleapis.com",
+    "cloudkms.googleapis.com"
+  ]
 }
